67a51ecaf25396e87adae31820d70de23622decc
Ishita Keshawani <keshwaniishita054@gmail.com>
2021-04-06 20:55:32 +0530

    adding files

---
+++ app.js
@@ -0,0 +1,30 @@
+var btntranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverUrl = "https://api.funtranslations.com/translate/ferb-latin.json"
+
+
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+}
+
+function clickHandler() {
+    // outputDiv.innerHTML = "hhjh"+txtInput.nodeValue;
+    var inputText = txtInput.value;
+
+    fetch(getTranslationUrl(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var tText = json.contents.translated;
+            outputDiv.innerText = tText;
+        })
+        .catch(errorHandler)
+
+};
+
+btntranslate.addEventListener("click", clickHandler)
