936629626b58190d0b53e9aefe82b34bebd78b51
Ishita Keshawani <keshwaniishita054@gmail.com>
2021-04-06 21:52:20 +0530

    add errorhandler

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ var btntranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverUrl = "https://api.funtranslations.com/translate/ferb-latin.json"
-
+// var serverUrl = "https://api.funtranslations.com/translate/ferb-latin.json"
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationUrl(text) {
     return serverUrl + "?" + "text=" + text
 }
 
 function errorHandler(error) {
-    console.log("error occured", error)
+    console.log("error occured", error);
+    alert("something wrong with server! try again after some time")
 }
 
 function clickHandler() {
